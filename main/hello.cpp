94473fd192b399468ce206281787b3a1ae4ffc2c
azhar-cs098 <azharameen.pes@gmail.com>
2023-02-17 09:51:11 +0530

    initial commit

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I, PES1UG20CS085, have successfully built and run\n";
+  cout << "Created change in file hello.cpp!\n";
+  return 0;
+}
